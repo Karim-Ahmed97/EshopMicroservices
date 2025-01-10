ae7cbcf7e263318a93d1a747a2657b4cf0db80d8
Karim Ahmed <kahmed@youxel.com>
2025-01-11 00:30:39 +0200

    create catalog api folders

--- Services/CatalogApi/CatalogApi/Program.cs
+++ Services/CatalogApi/CatalogApi/Program.cs
@@ -1,3 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+// Befor Building App -- Add Services 
+
+
+
 var app = builder.Build();
+// After Building App -- Add Middleware
+
+
+
 app.Run();
